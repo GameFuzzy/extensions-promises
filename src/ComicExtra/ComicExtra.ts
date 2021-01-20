f3e9323a9a61c016f34edbb92a9977e28379f883
GameFuzzy <awsmajed7@gmail.com>
2021-01-20 21:30:47 +0100

    Move parsing logic into separate file

--- src/ComicExtra/ComicExtra.ts
+++ src/ComicExtra/ComicExtra.ts
@@ -1,20 +1,21 @@
 import {
   Source,
   Manga,
-  MangaStatus,
   Chapter,
   ChapterDetails,
   HomeSection,
   MangaTile,
   SearchRequest,
-  LanguageCode,
   TagSection,
   PagedResults,
   SourceInfo,
   MangaUpdates,
   TagType,
-  Tag
 } from "paperback-extensions-common"
+
+import {
+  Parser,
+} from './Parser'
 
 const COMICEXTRA_DOMAIN = 'https://www.comicextra.com'
 
@@ -36,6 +37,7 @@ export const ComicExtraInfo: SourceInfo = {
 }
 
 export class ComicExtra extends Source {
+  parser = new Parser()
   getMangaShareUrl(mangaId: string): string | null { return `${COMICEXTRA_DOMAIN}/comic/${mangaId}` }
 
   async getMangaDetails(mangaId: string): Promise<Manga> {
@@ -44,92 +46,11 @@ export class ComicExtra extends Source {
       url: `${COMICEXTRA_DOMAIN}/comic/${mangaId}`,
       method: 'GET'
     })
-
     const data = await this.requestManager.schedule(request, 1)
 
-    let manga: Manga[] = []
     let $ = this.cheerio.load(data.data)
 
-    let titles = [$('.title-1', $('.mobile-hide')).text().trimStart()]
-    let image = $('img', $('.movie-l-img')).attr('src')
-
-    let summary = $('#film-content', $('#film-content-wrapper')).text().trim()
-    let relatedIds: string[] = []
-    for(let obj of $('.list-top-item').toArray()) {
-        relatedIds.push($('a', $(obj)).attr('href')?.replace(`${COMICEXTRA_DOMAIN}/comic/`, '')!.trim() || '')
-    }
-
-    let status = MangaStatus.ONGOING, author, released, rating: number = 0
-    let tagArray0 : Tag[] = []
-    let tagArray1 : Tag[] = []
-    let i = 0
-    for (let item of $('.movie-dd', $('.movie-dl')).toArray()) {
-      switch (i) {
-        case 0: {
-          //tagSections[1].tags.push(createTag({id: $(item).text().trim(), label: $(item).text().trim()}))
-          tagArray1 = [...tagArray1, createTag({id: $(item).text().trim(), label: $(item).text().trim()})]
-          i++
-          continue
-        }
-        case 1: {
-          // Comic Status
-          if ($('a', $(item)).text().toLowerCase().includes("ongoing")) {
-            status = MangaStatus.ONGOING
-          }
-          else {
-            status = MangaStatus.COMPLETED
-          }
-          i++
-          continue
-        }
-        case 2: {
-          // Alt Titles
-           if($(item).text().toLowerCase().trim() == "-") {
-            i++
-            continue
-           }
-           titles.push($(item).text().trim())
-          i++
-          continue
-        }
-        case 3: {
-          // Date of release
-          released = ($(item).text().trim()) ?? undefined
-          i++
-          continue
-        }
-        case 4: {
-          // Author
-          author = ($(item).text().trim()) ?? undefined
-          i++
-          continue
-          }
-        case 5: {
-          // Genres
-          for(let obj of $('a',$(item)).toArray()){
-            //tagSections[0].tags.push(createTag({id: $(obj).attr('href')?.replace(`${COMICEXTRA_DOMAIN}/`, '').trim()!, label: $(obj).text().trim()}))
-            tagArray0 = [...tagArray0, createTag({id: $(obj).attr('href')?.replace(`${COMICEXTRA_DOMAIN}/`, '').trim()!, label: $(obj).text().trim()})]
-          }    
-          i++
-          continue
-        }
-      }
-      i = 0
-    }
-    let tagSections: TagSection[] = [createTagSection({ id: '0', label: 'genres', tags: tagArray0 }),
-    createTagSection({ id: '1', label: 'format', tags: tagArray1 })]
-    return createManga({
-      id: mangaId,
-      rating: rating,
-      titles: titles,
-      image: image!,
-      status: status,
-      author: author,
-      tags: tagSections,
-      desc: summary,
-      lastUpdate: released,
-      relatedIds: relatedIds
-    })
+    return this.parser.parseMangaDetails($, mangaId)[0]
   }
 
 
@@ -154,36 +75,11 @@ export class ComicExtra extends Source {
       })
       const pageData = await this.requestManager.schedule(pageRequest, 1)
       $ = this.cheerio.load(pageData.data)
-      for(let obj of $('tr', $('#list')).toArray()) {
-          let chapterId = $('a', $(obj)).attr('href')?.replace(`${COMICEXTRA_DOMAIN}/${mangaId}/`, '')
-          let chapNum = chapterId?.replace(`chapter-`, '').trim()
-          if(isNaN(Number(chapNum))){
-            chapNum = `0.${chapNum?.replace( /^\D+/g, '')}`
-          }
-          let chapName = $('a', $(obj)).text()
-          let time = $($('td', $(obj)).toArray()[1]).text()
-
-          chapters.push(createChapter({
-              id: chapterId!,
-              mangaId: mangaId,
-              chapNum: Number(chapNum),
-              langCode: LanguageCode.ENGLISH,
-              name: chapName,
-              time: new Date(time)
-          }))
-      }
+      chapters = chapters.concat(this.parser.parseChapterList($, mangaId))
       pagesLeft--
     }
-    let sortedChapters: Chapter[] = []
     
-    // Sorts all the chapters and filters duplicates
-    chapters.forEach((c) => {
-      if (sortedChapters[sortedChapters.indexOf(c)]?.id !== c?.id) {
-        sortedChapters.push(c);
-      }
-    })
-    sortedChapters.sort((a, b) => (a.id > b.id) ? 1 : -1)
-    return sortedChapters
+    return this.parser.sortChapters(chapters)
   }
 
 
@@ -198,27 +94,7 @@ export class ComicExtra extends Source {
     const data = await this.requestManager.schedule(request, 1)
 
     let $ = this.cheerio.load(data.data)
-    let pages: string[] = []
-    console.log($('img',$('.chapter-container')).first().attr('src'))
-    if($('img',$('.chapter-container')).toArray().length < 1)
-    {
-      // Fallback to error image
-      pages.push('https://2.bp.blogspot.com/-Vc_P29M_7yk/WdSYg9e6F9I/AAAAAAAAEUI/3K5wt1yFlWEXfMZ6m6-haWMhN1HbjCWSACHMYCw/s0/RCO001.jpg')
-      console.log(`Fallback: ${pages[0]}`)
-    }
-    else {
-      // Get all of the pages
-      for(let obj of $('img',$('.chapter-container')).toArray()) {
-        pages.push($(obj).attr('src')!)
-      }
-    }
-
-    return createChapterDetails({
-      id: chapterId,
-      mangaId: mangaId,
-      pages: pages,
-      longStrip: false
-    })
+    return this.parser.parseChapterDetails($, mangaId, chapterId)
   }
 
   async filterUpdatedManga(mangaUpdatesFoundCallback: (updates: MangaUpdates) => void, time: Date, ids: string[]): Promise<void> {
@@ -234,41 +110,18 @@ export class ComicExtra extends Source {
       })
 
       let data = await this.requestManager.schedule(request, 1)
-
       let $ = this.cheerio.load(data.data)
 
-      let foundIds: string[] = []
-      let passedReferenceTime = false
-      for (let item of $('.hlb-t').toArray()) {
-        let id = ($('a', item).first().attr('href') ?? '')?.replace(`${COMICEXTRA_DOMAIN}/comic/`, '')!.trim() ?? ''
-        let mangaTime = new Date(time)
-        if($('.date', item).first().text().trim().toLowerCase() === "yesterday") {
-          mangaTime = new Date(Date.now())
-          mangaTime.setDate(new Date(Date.now()).getDate() - 1)
-        }
-        else {
-          mangaTime = new Date($('.date', item).first().text()) 
-        }
-        passedReferenceTime = mangaTime <= time
-        if (!passedReferenceTime) {
-          if (ids.includes(id)) {
-            foundIds.push(id)
-          }
-        }
-        else break
-      }
-
-      if (!passedReferenceTime) {
+      let updatedComics = this.parser.filterUpdatedManga($, time, ids)
+      loadNextPage = updatedComics.loadNextPage
+      if (loadNextPage) {
         currPageNum++
       }
-
-      else {
-        loadNextPage = false
-      }
-
+      if (updatedComics.updates.length > 0) {
       mangaUpdatesFoundCallback(createMangaUpdates({
-        ids: foundIds
+        ids: updatedComics.updates
       }))
+      }
     }
   }
 
@@ -280,26 +133,10 @@ export class ComicExtra extends Source {
     })
 
     const data = await this.requestManager.schedule(request, 1)
-
     let $ = this.cheerio.load(data.data)
-    let mangaTiles: MangaTile[] = []
-
-    for(let obj of $('.cartoon-box').toArray()) {
-      let id = $('a', $(obj)).attr('href')?.replace(`${COMICEXTRA_DOMAIN}/comic/`, '')
-      let titleText = $('h3', $(obj)).text()
-      let image = $('img', $(obj)).attr('src')
-
-      if(titleText == "Not found") continue // If a search result has no data, the only cartoon-box object has "Not Found" as title. Ignore.
-
-      mangaTiles.push(createMangaTile({
-          id: id!,
-          title: createIconText({text: titleText}),
-          image: image!
-      }))
-  }
 
     return createPagedResults({
-      results: mangaTiles
+      results: this.parser.parseSearchResults($)
     })
 
   }
@@ -314,17 +151,7 @@ export class ComicExtra extends Source {
     const data = await this.requestManager.schedule(request, 1)
     let $ = this.cheerio.load(data.data)
 
-    let tagSections: TagSection[] = [createTagSection({ id: '0', label: 'genres', tags: [] }),
-    createTagSection({ id: '1', label: 'format', tags: [] })]
-
-    for(let obj of $('a', $('.home-list')).toArray()) {
-      let id = $(obj).attr('href')?.replace(`${COMICEXTRA_DOMAIN}/`, '')!.trim() ?? $(obj).text().trim()
-      let genre = $(obj).text().trim()
-      tagSections[0].tags.push(createTag({id: id, label: genre}))
-    }
-    tagSections[1].tags.push(createTag({id: 'comic/', label: 'Comic'}))
-
-    return tagSections
+    return this.parser.parseTags($)
   }
 
 
@@ -345,27 +172,10 @@ export class ComicExtra extends Source {
     })
 
     const popularData = await this.requestManager.schedule(request, 1)
-
-    let popular: MangaTile[] = []
     let $ = this.cheerio.load(popularData.data)
 
-    for(let obj of $('.cartoon-box').toArray()) {
-      let id = $('a', $(obj)).attr('href')?.replace(`${COMICEXTRA_DOMAIN}/comic/`, '')
-      let title = $('h3', $(obj)).text().trim()
-      let image = $('img', $(obj)).attr('src')
-
-      popular.push(createMangaTile({
-          id: id!,
-          title: createIconText({text: title}),
-          image: image!
-      }))
-  }
-
-    popularSection.items = popular
+    popularSection.items = this.parser.parseHomePageSection($)
     sectionCallback(popularSection)
-
-
-    let recent: MangaTile[] = []
 
     request = createRequestObject({
       url: `${COMICEXTRA_DOMAIN}/recent-comic`,
@@ -375,22 +185,8 @@ export class ComicExtra extends Source {
     const recentData = await this.requestManager.schedule(request, 1)
     $ = this.cheerio.load(recentData.data)
 
-    for(let obj of $('.cartoon-box').toArray()) {
-      let id = $('a', $(obj)).attr('href')?.replace(`${COMICEXTRA_DOMAIN}/comic/`, '')
-      let title = $('h3', $(obj)).text().trim()
-      let image = $('img', $(obj)).attr('src')
-
-      recent.push(createMangaTile({
-          id: id!,
-          title: createIconText({text: title}),
-          image: image!
-      }))
-  }
-
-    recentSection.items = recent
+    recentSection.items = this.parser.parseHomePageSection($)
     sectionCallback(recentSection)
-
-    let newTitles: MangaTile[] = []
 
     request = createRequestObject({
       url: `${COMICEXTRA_DOMAIN}/new-comic`,
@@ -400,35 +196,24 @@ export class ComicExtra extends Source {
     const newData = await this.requestManager.schedule(request, 1)
     $ = this.cheerio.load(newData.data)
 
-    for(let obj of $('.cartoon-box').toArray()) {
-      let id = $('a', $(obj)).attr('href')?.replace(`${COMICEXTRA_DOMAIN}/comic/`, '')
-      let title = $('h3', $(obj)).text().trim()
-      let image = $('img', $(obj)).attr('src')
-
-      newTitles.push(createMangaTile({
-          id: id!,
-          title: createIconText({text: title}),
-          image: image!
-      }))
-    }
-
-    newTitlesSection.items = newTitles
+    newTitlesSection.items = this.parser.parseHomePageSection($)
     sectionCallback(newTitlesSection)
   }
   
+
   async getViewMoreItems(homepageSectionId: string, metadata: any): Promise<PagedResults | null> {
     let page = ''
     switch (homepageSectionId) {
-      case '2': {
-        page = `/popular-comic/${metadata.page ? metadata.page : 1}`
+      case '0': {
+        page = `/new-comic/${metadata.page ? metadata.page : 1}`
         break
       }
       case '1': {
         page = `/recent-comic/${metadata.page ? metadata.page : 1}`
         break
       }
-      case '0': {
-        page = `/new-comic/${metadata.page ? metadata.page : 1}`
+      case '2': {
+        page = `/popular-comic/${metadata.page ? metadata.page : 1}`
         break
       }
       default: return Promise.resolve(null)
@@ -443,21 +228,7 @@ export class ComicExtra extends Source {
     let data = await this.requestManager.schedule(request, 1)
 
     let $ = this.cheerio.load(data.data)
-    //let manga: MangaTile[] = []
-    let manga: Set<MangaTile> = new Set<MangaTile>()
-
-    for(let obj of $('.cartoon-box').toArray()) {
-      let id = $('a', $(obj)).attr('href')?.replace(`${COMICEXTRA_DOMAIN}/comic/`, '')
-      let title = $('h3', $(obj)).text().trim()
-      let image = $('img', $(obj)).attr('src')
-      console.log(`ADDING ${id!}`)
-      manga.add(createMangaTile({
-          id: id!,
-          title: createIconText({text: title}),
-          image: image!
-      }))
-      console.log(manga)
-    }
+    let manga = this.parser.parseHomePageSection($)
 
     /*if (!this.isLastPage($)) {
       metadata.page ? metadata.page++ : metadata.page = 2
