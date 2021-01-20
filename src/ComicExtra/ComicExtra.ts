1216402148e12d8c818834bbbe0b160fbec6a1d4
GameFuzzy <30367459+GameFuzzy@users.noreply.github.com>
2021-01-20 12:37:50 +0100

    Changed version number

--- src/ComicExtra/ComicExtra.ts
+++ src/ComicExtra/ComicExtra.ts
@@ -18,7 +18,7 @@ import {
 const COMICEXTRA_DOMAIN = 'https://www.comicextra.com'
 
 export const ComicExtraInfo: SourceInfo = {
-  version: '1.2.4',
+  version: '1.2.5',
   name: 'ComicExtra',
   description: 'Extension that pulls western comics from ComicExtra.com',
   author: 'GameFuzzy',
